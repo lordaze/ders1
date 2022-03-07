c8cdfa6e7fba610237b44e8e637ad0e0bf326871
lordaze <101128746+lordaze@users.noreply.github.com>
2022-03-07 21:50:40 +0300

    asda

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("SOX ICIVE!");
-Console.WriteLine("SOX ICIVs amcello!");
+Console.WriteLine("SOIVE!");
