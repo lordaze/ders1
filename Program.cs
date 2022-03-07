8729ae61edf15c8f9b7ff964caaef8addb0441d6
lordaze <101128746+lordaze@users.noreply.github.com>
2022-03-07 21:41:02 +0300

    lox2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("SOX ICIVE!");
-Console.WriteLine("SOX ICIVs!");
+Console.WriteLine("SOX ICIVs amcello!");
